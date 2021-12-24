0c678df1c0276de128f7943e913e5cdeabd4b778
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2021-12-24 17:19:16 +0530

    data fetching from backend -> frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-
-import './App.css';
+import "./App.css";
+import { Route } from "react-router-dom";
+import homePage from "./Pages/homePage";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
-    <div className="App">Hello</div>);
+    <div className="App">
+      <Route path="/" component={homePage} exact />
+      <Route path="/chats" component={ChatPage} />
+    </div>
+  );
 }
 
 export default App;
