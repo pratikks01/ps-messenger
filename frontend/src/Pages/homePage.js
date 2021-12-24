418cd367cb55ca7400b65865c9b9e5117cd8d004
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2021-12-24 20:50:44 +0530

    Homepage and Signup menu created

--- frontend/src/Pages/homePage.js
+++ frontend/src/Pages/homePage.js
@@ -1,6 +1,58 @@
 import React from "react";
+import {
+  Text,
+  Box,
+  Container,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tab,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
-const homePage = () => {
-  return <div>Home Page</div>;
+const HomePage = () => {
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="3xl" fontFamily="Work sans" color="black">
+          Ps-Messenger
+        </Text>
+      </Box>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
-export default homePage;
+export default HomePage;
