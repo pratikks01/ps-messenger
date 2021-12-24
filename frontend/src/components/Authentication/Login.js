62d0532d91c269ec28874e1b6a2eea1b1ea6c7a7
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2021-12-24 21:20:18 +0530

    Created Login page

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,7 +1,64 @@
-import React from "react";
+import { VStack } from "@chakra-ui/layout";
+import { Button, FormControl, FormLabel, Show } from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  const [show, setShow] = useState(false);
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const handleClick = () => setShow(!show);
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="5px" color="black">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter you Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Login
+      </Button>
+
+      <Button
+        varient="solid"
+        colorScheme="red"
+        width="100%"
+        onClick={() => {
+          setEmail("guest@example.com");
+          setPassword("12345678");
+        }}
+      >
+        Get Guest Login Credentials
+      </Button>
+    </VStack>
+  );
 };
 
 export default Login;
