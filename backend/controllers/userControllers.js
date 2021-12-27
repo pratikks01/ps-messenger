a99d5a539de515659c7ff9c7af4600ba37658caf
Pratik Shastrakar <48235970+pstricks01@users.noreply.github.com>
2021-12-27 17:24:28 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -24,8 +24,8 @@ const registerUser = asyncHandler(async (req, res) => {
   if (User) {
     res.status(201).json({
       _id: user._id,
-      name: user._id,
-      email: user.name,
+      name: user.name,
+      email: user.email,
       pic: user.pic,
       token: generateToken(user._id),
     });
@@ -43,8 +43,8 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
-      name: user._id,
-      email: user.name,
+      name: user.name,
+      email: user.email,
       pic: user.pic,
       token: generateToken(user._id),
     });
